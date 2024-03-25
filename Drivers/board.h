8d921cb7f56a450369b61c5e84c678488bc8cff0
qing <15007971764@163.com>
2024-03-25 15:45:27 +0800

    Solving the hard fault handle issue: During heap space initialization, there may be conflicts with the stack space.

--- Drivers/board.h
+++ Drivers/board.h
@@ -37,9 +37,19 @@ extern int Image$$RW_IRAM1$$ZI$$Limit;
 #else
 extern int __bss_end;
 #define HEAP_BEGIN      ((void *)&__bss_end)
+/* Heap size[Kbytes] <8-16>, Default: 16*/
+#define STM32_HEAP_SIZE      16
+#define STM32_HEAP_END       ((void *)((int)&__bss_end + STM32_HEAP_SIZE * 1024))
 #endif
 
-#define HEAP_END        STM32_SRAM_END
+/*
+ * The initial address of sp is _estack = STM32_SRAM_END, so the end address of the heap should not be the same here.
+ * Otherwise, during the initialization of the heap space, it may conflict with the stack space,
+ * resulting in a change in the value of the LR register that has already been saved in the stack,
+ * and finally PC returning to an unexpected address. Therefore, a hard fault handle is generated.
+ */
+// #define HEAP_END        STM32_SRAM_END
+#define HEAP_END        STM32_HEAP_END
 
 void SystemClock_Config(void);
 
